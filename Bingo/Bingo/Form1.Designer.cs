7be8815989a09a9a2aa248c9f0b8081de9e7127d
Fransuapolanco <48096892+Fransuapolanco@users.noreply.github.com>
2019-10-24 19:23:10 -0600

    Add files via upload

---
+++ Bingo/Bingo/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Bingo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            resources.ApplyResources(this.button3, "button3");
+            this.button3.Name = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            resources.ApplyResources(this.button4, "button4");
+            this.button4.Name = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            resources.ApplyResources(this.button5, "button5");
+            this.button5.Name = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Name = "label5";
+            // 
+            // label6
+            // 
+            resources.ApplyResources(this.label6, "label6");
+            this.label6.Name = "label6";
+            // 
+            // label7
+            // 
+            resources.ApplyResources(this.label7, "label7");
+            this.label7.Name = "label7";
+            // 
+            // label8
+            // 
+            resources.ApplyResources(this.label8, "label8");
+            this.label8.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.label8.Name = "label8";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Bingo/Bingo/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Speech.Synthesis;
+
+namespace Bingo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = ".";
+            label2.Text = ".";
+            label3.Text = ".";
+            label4.Text = ".";
+            label5.Text = ".";
+            label6.Text = ".";
+            label7.Text = ".";
+        }
+        int currentNumber;
+        List<int> pastNumber = new List<int>();
+        SpeechSynthesizer syn = new SpeechSynthesizer();
+        Task t;
+        bool pause_ = true;
+
+        void GenerateNumber()
+            {
+                Random rnd = new Random();
+                do
+                {
+                    currentNumber = rnd.Next(0, 99);
+                }
+                while (pastNumber.Contains(currentNumber));
+                pastNumber.Add(currentNumber);
+
+                if(InvokeRequired)
+                {
+                    this.BeginInvoke(new MethodInvoker(delegate
+                    {
+                        label1.Refresh();
+                        label1.Text = currentNumber.ToString();
+
+                        if (pastNumber.Count == 2)
+                        {
+                            label7.Text = pastNumber[pastNumber.Count - 2].ToString();
+                        }
+                        else if (pastNumber.Count == 3)
+                        {
+                            label6.Text = pastNumber[pastNumber.Count - 3].ToString();
+                            label7.Text = pastNumber[pastNumber.Count - 2].ToString();
+                        }
+                        else if (pastNumber.Count == 4)
+                        {
+                            label5.Text = pastNumber[pastNumber.Count - 4].ToString();
+                            label6.Text = pastNumber[pastNumber.Count - 3].ToString();
+                            label7.Text = pastNumber[pastNumber.Count - 2].ToString();
+
+                        }
+                        else if (pastNumber.Count == 5)
+                        {
+                            label4.Text = pastNumber[pastNumber.Count - 5].ToString();
+                            label5.Text = pastNumber[pastNumber.Count - 4].ToString();
+                            label6.Text = pastNumber[pastNumber.Count - 3].ToString();
+                            label7.Text = pastNumber[pastNumber.Count - 2].ToString();
+                        }
+                        else if (pastNumber.Count == 6)
+                        {
+                            label3.Text = pastNumber[pastNumber.Count - 6].ToString();
+                            label4.Text = pastNumber[pastNumber.Count - 5].ToString();
+                            label5.Text = pastNumber[pastNumber.Count - 4].ToString();
+                            label6.Text = pastNumber[pastNumber.Count - 3].ToString();
+                            label7.Text = pastNumber[pastNumber.Count - 2].ToString();
+                        }
+                        else if (pastNumber.Count > 7 || pastNumber.Count == 7)
+                        {
+                            label2.Text = pastNumber[pastNumber.Count - 7].ToString();
+                            label3.Text = pastNumber[pastNumber.Count - 6].ToString();
+                            label4.Text = pastNumber[pastNumber.Count - 5].ToString();
+                            label5.Text = pastNumber[pastNumber.Count - 4].ToString();
+                            label6.Text = pastNumber[pastNumber.Count - 3].ToString();
+                            label7.Text = pastNumber[pastNumber.Count - 2].ToString();
+                        }
+
+                        if (pastNumber.Count == 99)
+                        {
+                            MessageBox.Show("Finished");
+
+                        }
+
+                    }));
+                }
+
+                syn.Speak("The current number is" + currentNumber.ToString());
+
+            }
+            private void button1_Click(object sender, EventArgs e)
+        {
+            t = Task.Factory.StartNew(() => {
+            
+                for (int i=0;i<100;i++)
+                {
+                    Thread.Sleep(1000);
+                    if(pause_==false)
+                    {
+                        pause_ = true;
+                        t.Wait();
+                    }
+                    GenerateNumber();
+
+                }
+            
+            });
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            pause_ = false;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //clear pastnumber list
+            pastNumber.Clear();
+            pause_ = false;
+            button2.Text = "Start_manual";
+            label1.Text = ".";
+            label2.Text = ".";
+            label3.Text = ".";
+            label4.Text = ".";
+            label5.Text = ".";
+            label6.Text = ".";
+            label7.Text = ".";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.Text = "NEXT";
+            Task.Factory.StartNew(() =>
+            {
+                GenerateNumber();
+            });
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
